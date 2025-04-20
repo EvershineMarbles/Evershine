fe97ff2dd11e45522a2ff2179e03d29838f9000e
Sonal Baheti <sonal.21mcan176@jecrcu.edu.in>
2025-04-20 13:14:51 +0530

    update

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -82,7 +82,7 @@ export default function QRCodeGenerator({
         qrCode.crossOrigin = "anonymous"
         qrCode.onload = () => {
           // Draw QR code in the white space at bottom right - adjusted position
-          ctx.drawImage(qrCode, 350, 600, 150, 150)
+          ctx.drawImage(qrCode, 380, 630, 150, 150)
 
           // Convert canvas to data URL
           const dataUrl = canvas.toDataURL("image/png")
