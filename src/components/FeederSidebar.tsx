0761cb67aac221dcb2bedff1f2af2860235558bf
Sonal Baheti <sonal.21mcan176@jecrcu.edu.in>
2025-05-13 13:05:29 +0530

    update

--- src/components/FeederSidebar.tsx
+++ src/components/FeederSidebar.tsx
@@ -43,7 +43,7 @@ export function FeederSidebar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-black text-white shadow-lg">
       {/* Blue top strip */}
-      <div className="w-full h-10 bg-[#194a95]"></div>
+      <div className="w-full h-6 bg-[#194a95]"></div>
 
       <div className="sidebar-icon mt-4">
         <span className="text-xl font-bold">F</span>
